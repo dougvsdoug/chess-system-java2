f5cae5b7e6bfd64f0bfb6a2a7edcd36da2e42ae9
dougvsdoug <dougvsdoug@gmail.com>
2019-02-20 18:15:33 -0300

    Implementing possible moves of king

--- src/application/UI.java
+++ src/application/UI.java
@@ -76,7 +76,7 @@ public class UI {
 		}
 		
 		System.out.println("  a b c d e f g h");
-		
+		 
 	}
 	
 	public static void printBoard( ChessPiece[][] pieces, boolean[][] possibleMoves ) {// sobrecarga
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -16,9 +17,16 @@ public class King extends ChessPiece{
 	public String toString() {
 		return "K";
 	}
-
+	
+	private boolean canMove( Position position ) {//diz se o rei pode mover para uma determinada posição
+		ChessPiece p = (ChessPiece) getBoard().piece(position);//note q a posição já foi validada pelo método
+		// piece
+		return p == null || p.getColor() != getColor();// acho q aqui se p == null ele nem testa o p.getcolor
+		// acho q daria NullPointerException
+	}
+	
 	@Override
-	public boolean[][] possibleMoves() {
+	public boolean[][] possibleMoves() {// retorna uma matriz com todos os movimentos possíveis da peça
 		//Obs: é obrigatório implementar esse método pq ele é um método herdado q era abstrato
 		// Obs: por padrão todas as posições de uma matriz de boolean começam com falso
 		
@@ -26,6 +34,75 @@ public class King extends ChessPiece{
 		//getBoard().getRows, getBoard retorna um objeto da classe Board e nós utilizamos o método
 		// getRows desse objeto da classe Board
 		// getBoard foi herdado da classe Piece
+		// note q quando instancia uma matriz de boolean, as posiões são preenchidas com false por padrão
+	
+		Position p = new Position(0, 0);// p é uma var auxiliar, ela começa com 0 pq é um valor qualquer
+		//eles serão mudados depois, pelo método setValues
+		
+		//above
+		
+		p.setValues( position.getRow() - 1, position.getColumn() );// position.get é a posição de origem da peça
+		
+		if( getBoard().positionExists(p) && canMove(p) ){
+			mat[p.getRow()][p.getColumn()] = true;
+		}	
+		
+		//below
+		
+		p.setValues( position.getRow() + 1, position.getColumn() );// position.get é a posição de origem da peça
+		
+		if( getBoard().positionExists(p) && canMove(p) ){
+			mat[p.getRow()][p.getColumn()] = true;
+		}	
+		
+		//left
+
+		p.setValues( position.getRow(), position.getColumn() - 1 );// position.get é a posição de origem da peça
+		
+		if( getBoard().positionExists(p) && canMove(p) ){
+			mat[p.getRow()][p.getColumn()] = true;
+		}	
+
+		//right
+
+		p.setValues( position.getRow(), position.getColumn() + 1 );// position.get é a posição de origem da peça
+		
+		if( getBoard().positionExists(p) && canMove(p) ){
+			mat[p.getRow()][p.getColumn()] = true;
+		}			
+		
+		//northwest
+
+		p.setValues( position.getRow() - 1, position.getColumn() - 1 );// position.get é a posição de origem da peça
+		
+		if( getBoard().positionExists(p) && canMove(p) ){
+			mat[p.getRow()][p.getColumn()] = true;
+		}	
+		
+		//northeast
+
+		p.setValues( position.getRow() - 1, position.getColumn() + 1 );// position.get é a posição de origem da peça
+		
+		if( getBoard().positionExists(p) && canMove(p) ){
+			mat[p.getRow()][p.getColumn()] = true;
+		}	
+		
+		//south-west
+
+		p.setValues( position.getRow() + 1, position.getColumn() - 1 );// position.get é a posição de origem da peça
+		
+		if( getBoard().positionExists(p) && canMove(p) ){
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//south-east
+
+		p.setValues( position.getRow() + 1, position.getColumn() + 1 );// position.get é a posição de origem da peça
+		
+		if( getBoard().positionExists(p) && canMove(p) ){
+			mat[p.getRow()][p.getColumn()] = true;
+		}	
+		
 		return mat;
 	}
 }
